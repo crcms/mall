e649d3b3c5b922de55819ebc1da98902120b6bc2
crcms <crcms@crcms.cn>
2018-07-16 06:49:43 +0800

    Migration update

--- database/migrations/2018_07_08_152555_mall_product_categories.php
+++ database/migrations/2018_07_08_152555_mall_product_categories.php
@@ -16,14 +16,14 @@ class MallProductCategories extends Migration
         Schema::create('mall_product_categories', function (Blueprint $table) {
             $table->unsignedBigInteger('id',true);
             $table->nestedSet();
-            $table->string('name',50)->default('')->comment('分类名称');
+            $table->string('name',50)->comment('分类名称');
             $table->string('sign',50)->nullable()->comment('分类标识');
-            $table->unsignedInteger('status')->default(0)->commet('状态，1开启，2隐藏，3关闭');
+            $table->unsignedTinyInteger('status')->default(0)->commet('状态，1开启，2隐藏，3关闭');
             $table->unsignedBigInteger('sort')->default(0)->comment('排序值');
             $table->string('icon',255)->default('')->comment('图标路径');
             $table->unsignedBigInteger('created_at')->default(0)->comment('创建时间');
             $table->unsignedBigInteger('updated_at')->default(0)->comment('修改时间');
-            $table->unsignedBigInteger('deleted_at')->default(null)->nullable()->comment('删除时间');
+            $table->unsignedBigInteger('deleted_at')->nullable()->comment('删除时间');
 
             $table->unique('sign');
         });
--- database/migrations/2018_07_14_110356_mall_product_specifications_table.php
+++ database/migrations/2018_07_14_110356_mall_product_specifications_table.php
@@ -16,12 +16,12 @@ class MallProductSpecificationsTable extends Migration
         Schema::create('mall_product_specifications', function (Blueprint $table) {
             $table->unsignedBigInteger('id',true);
             $table->unsignedBigInteger('category_id')->default(0)->comment('分类id,0表示全局共用');
-            $table->string('name',50)->default('')->comment('规格名称');
-            $table->unsignedInteger('status')->default(0)->commet('状态，1开启，2隐藏，3关闭');
+            $table->string('name',50)->comment('规格名称');
+            $table->unsignedTinyInteger('status')->default(0)->commet('状态，1开启，2隐藏，3关闭');
             $table->unsignedBigInteger('sort')->default(0)->comment('排序值');
             $table->unsignedBigInteger('created_at')->default(0)->comment('创建时间');
             $table->unsignedBigInteger('updated_at')->default(0)->comment('修改时间');
-            $table->unsignedBigInteger('deleted_at')->default(null)->nullable()->comment('删除时间');
+            $table->unsignedBigInteger('deleted_at')->nullable()->comment('删除时间');
         });
     }
 
