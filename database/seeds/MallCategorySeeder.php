b176a21d02930f6f2637dcb1a0fe676b853e03a9
crcms <crcms@crcms.cn>
2018-07-18 20:59:12 +0800

    CategoryModel renamed ProductCategoryModel

--- database/seeds/MallCategorySeeder.php
+++ database/seeds/MallCategorySeeder.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use CrCms\Mall\Models\CategoryModel;
+use CrCms\Mall\Models\ProductCategoryModel;
 use CrCms\Mall\Attributes\MallAttribute;
 
 class MallCategorySeeder extends Seeder
@@ -14,11 +14,11 @@ class MallCategorySeeder extends Seeder
      */
     public function run()
     {
-        DB::table((new CategoryModel())->getTable())->truncate();
+        DB::table((new ProductCategoryModel())->getTable())->truncate();
 
         $mainIds = [];
         for ($i = 1;$i<=10;$i++) {
-            $mainIds[] = CategoryModel::create([
+            $mainIds[] = ProductCategoryModel::create([
                 'name' => '一级分类'.strval($i),
                 'status' => MallAttribute::STATUS_ENABLE,
                 'sort' => mt_rand(1,9999),
@@ -28,7 +28,7 @@ class MallCategorySeeder extends Seeder
         $twoIds = [];
         foreach ($mainIds as $mid) {
             for ($i = 1;$i<=15;$i++) {
-                $twoIds[] = CategoryModel::create([
+                $twoIds[] = ProductCategoryModel::create([
                     'name' => '二级分类'.strval($i),
                     'status' => MallAttribute::STATUS_ENABLE,
                     'sort' => mt_rand(1,9999),
@@ -39,7 +39,7 @@ class MallCategorySeeder extends Seeder
 
         foreach ($twoIds as $sid) {
             for ($i = 1;$i<=15;$i++) {
-                CategoryModel::create([
+                ProductCategoryModel::create([
                     'name' => '三级分类'.strval($i),
                     'status' => MallAttribute::STATUS_ENABLE,
                     'sort' => mt_rand(1,9999),
