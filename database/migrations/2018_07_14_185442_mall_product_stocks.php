a109d45b75250af9400ff7718878b608709b3056
crcms <crcms@crcms.cn>
2018-07-18 20:58:25 +0800

    Inventory data table format modification

--- database/migrations/2018_07_14_185442_mall_product_stocks.php
+++ database/migrations/2018_07_14_185442_mall_product_stocks.php
@@ -16,11 +16,11 @@ class MallProductStocks extends Migration
         Schema::create('mall_product_stocks', function (Blueprint $table) {
             $table->unsignedBigInteger('id', true);
             $table->unsignedBigInteger('product_id')->default(0)->comment('商品ID');
-            //$table->unsignedBigInteger('specification_id')->default(0)->comment('规则ID');
+            $table->unsignedBigInteger('specification_id')->default(0)->comment('规则ID');
             $table->decimal('market_price', 10, 2)->default(0)->comment('市场价格');
             $table->decimal('price', 10, 2)->default(0)->comment('价格');
             $table->decimal('cost_price', 10, 2)->default(0)->comment('成本价格');
-            $table->json('specification_value')->comment('规格值');
+            $table->string('specification_value', 30)->comment('规格值');
             $table->unsignedInteger('stock')->default(0)->comment('库存');
             $table->unsignedInteger('stock_alarm')->default(0)->comment('库存预警');
             $table->string('image', 150)->nullable()->comment('图片地址');
