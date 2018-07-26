b8afee96b0071e764407c86a5778197ac783b6a7
crcms <crcms@crcms.cn>
2018-07-27 07:29:20 +0800

    SpecificationActions rename SpecificationHandlers

---
+++ src/Handlers/Product/Manage/Specification/StoreHandler.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * @author simon <crcms@crcms.cn>
+ * @datetime 2018-07-14 11:24
+ * @link http://crcms.cn/
+ * @copyright Copyright &copy; 2018 Rights Reserved CRCMS
+ */
+
+namespace CrCms\Mall\Handlers\Product\Manage\Specification;
+
+use CrCms\Foundation\App\Handlers\AbstractHandler;
+use CrCms\Foundation\App\Handlers\Traits\RepositoryHandlerTrait;
+use CrCms\Foundation\App\Handlers\Traits\RequestHandlerTrait;
+use CrCms\Mall\Models\ProductSpecificationModel;
+use CrCms\Mall\Repositories\ProductSpecificationRepository;
+use Illuminate\Http\Request;
+
+/**
+ * Class StoreHandler
+ * @package CrCms\Mall\Handlers\Product\Manage\Specification
+ */
+class StoreHandler extends AbstractHandler
+{
+    use RequestHandlerTrait, RepositoryHandlerTrait;
+
+    /**
+     * StoreHandler constructor.
+     * @param Request $request
+     * @param ProductSpecificationRepository $repository
+     */
+    public function __construct(Request $request, ProductSpecificationRepository $repository)
+    {
+        $this->request = $request;
+        $this->repository = $repository;
+    }
+
+    /**
+     * @param array $data
+     * @return ProductSpecificationModel
+     */
+    public function handle(array $data = []): ProductSpecificationModel
+    {
+        //$this->validate();
+
+        return $this->repository->create($this->request->all());
+    }
+}
---
+++ src/Handlers/Product/Manage/Specification/UpdateHandler.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * @author simon <crcms@crcms.cn>
+ * @datetime 2018-07-14 11:26
+ * @link http://crcms.cn/
+ * @copyright Copyright &copy; 2018 Rights Reserved CRCMS
+ */
+
+namespace CrCms\Mall\Handlers\Product\Manage\Specification;
+
+use CrCms\Foundation\App\Handlers\AbstractHandler;
+use CrCms\Foundation\App\Handlers\Traits\RepositoryHandlerTrait;
+use CrCms\Foundation\App\Handlers\Traits\RequestHandlerTrait;
+use CrCms\Mall\Models\ProductSpecificationModel;
+use CrCms\Mall\Repositories\ProductSpecificationRepository;
+use Illuminate\Http\Request;
+
+/**
+ * Class UpdateHandler
+ * @package CrCms\Mall\Handlers\Product\Manage\Specification
+ */
+class UpdateHandler extends AbstractHandler
+{
+    use RequestHandlerTrait, RepositoryHandlerTrait;
+
+    /**
+     * UpdateAction constructor.
+     * @param Request $request
+     * @param ProductSpecificationRepository $repository
+     */
+    public function __construct(Request $request, ProductSpecificationRepository $repository)
+    {
+        $this->request = $request;
+        $this->repository = $repository;
+    }
+
+    /**
+     * @return ProductSpecificationModel
+     */
+    public function handle(): ProductSpecificationModel
+    {
+        //$this->validate();
+
+        return $this->repository->update($this->request->all(), $this->request->route()->parameter('specification'));
+    }
+}
